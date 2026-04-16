bead19e37b08bcec9bd25d6512d47fdff9bcc385
harshshahu <72179348+harshshahu@users.noreply.github.com>
2026-04-16 05:37:06 +0530

    Fix output variable names in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "instance_public_ip" {
   description = "Public IP address of the Jenkins instance"
-  value = aws_instance.Jenkins-VM.public_ip
+  value = aws_instance.Jenkins_VM.public_ip
 }
 
 output "ssh_connection_command" {
   description = ""
-  value = "ssh -i jenkins_private_key.pem ec2-user@${aws_insance.Jenkins-VM.public_ip}"
+  value = "ssh -i jenkins_private_key.pem ec2-user@${aws_instance.Jenkins_VM.public_ip}"
 }
 
-output "jenkins-vm_admin_paddword_command" {
+output "jenkins_vm_admin_password_command" {
   description = ""
-  value = "ssh -i jenkins_private_key.pem ec2-user@${aws_instance.Jenkins-VM.public_ip} 'sudo cat /var/lib/jenkins/secrets/initialAdminPassword'"
+  value = "ssh -i jenkins_private_key.pem ec2-user@${aws_instance.Jenkins_VM.public_ip} 'sudo cat /var/lib/jenkins/secrets/initialAdminPassword'"
 }
 
-output "jenkins-vm_url" {
+output "jenkins_vm_url" {
   description = "URL to access Jenkins"
-  value = "http://${aws_instance.Jenkins-VM.public_ip}:8080"
+  value = "http://${aws_instance.Jenkins_VM.public_ip}:8080"
 }
